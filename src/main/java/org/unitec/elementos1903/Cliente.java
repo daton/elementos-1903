bddfdea3460a537d3f8f1772c9ab332f7dd747e7
campitos <rapidclimate@gmail.com>
2019-06-04 21:29:55 -0500

    aaaa

---
+++ src/main/java/org/unitec/elementos1903/Cliente.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos1903;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author campitos
+ */
+public class Cliente {
+    
+    
+   @Id
+    private Integer id;
+   
+    private String nombre;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + '}';
+    }
+    
+    
+
+    public Cliente(Integer id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Cliente() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
--- src/main/java/org/unitec/elementos1903/Elementos1903Application.java
+++ src/main/java/org/unitec/elementos1903/Elementos1903Application.java
@@ -1,13 +1,40 @@
 package org.unitec.elementos1903;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Elementos1903Application {
+public class Elementos1903Application implements CommandLineRunner{
 
+    @Autowired
+    RepoCliente repoCliente;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Elementos1903Application.class, args);
+                
+                
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+      // repoCliente.save(new Cliente(1, "daton", "rapidclimate@gmail.com"));
+     // repoCliente.save(new Cliente(2, "Topoyiyo", "yo@gmil.com"));
+     
+     //Buscar todos
+     
+     for(Cliente c: repoCliente.findAll()){
+         System.out.println(c);
+     }
+     
+     //Buscar por id
+        System.out.println(repoCliente.findById(2));
+        
+      //Borramos el primero despues lo vuelven a insertar
+  //  repoCliente.deleteById(1);
+        
+    }
 
 }
